800ff83716d2352bc43f3932abf9539be18b8c41
aadejarde15@gmail.com <aadejarde15@gmail.com>
2022-07-16 16:18:01 -0300

    configuração de duração do tempo da tela de carregamento

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:projeto_poke/view/app_widgets.dart';
 
 void main() {
+  await initialization(null);
   runApp(const MyApp());
+}
+
+Future initialization(BuildContext? context) async {
+
+  await Future.delayed(Duration(seconds: 3));
 }
 
 
